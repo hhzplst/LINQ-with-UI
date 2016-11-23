94a8950963d5872c61ccb2d8a17de7ec7a342247
Ivy Chang <ivycwq@icloud.com>
2016-11-23 15:17:32 -0800

    adding Book class

---
+++ Book.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace LINQwithUI {
+  public class Book {
+    private static int nextID = 1000;
+    public int ID {get {return nextID;} private set {ID = nextID++;}}
+    public int Year {get; set;}
+    public string Title {get;set;}
+    public string Publisher {get; set;}
+    public string[] Authors {get; set;}
+    public List<string> SubjectHeadings {get; set;}
+    public bool IfAvailable {get;}
+    public int BorrowerID {get;}
+    
+    public override string ToString() {
+      return string.Format("{0, -5}{1, -5}{2, -20}{3, -20}", ID, Year, Title, string.Join(", ", Authors));
+    }
+  }
+}
