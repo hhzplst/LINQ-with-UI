6934b5d69cf6081ccd659ff380fe9b1d15328613
Ivy Chang <ivycwq@icloud.com>
2016-11-23 16:59:32 -0800

    testing

--- Library.cs
+++ Library.cs
@@ -37,5 +37,17 @@ namespace LINQwithUI {
       foreach (Book b in bookQuery)
         Console.WriteLine("\t" + b);
     }
+    public static void Main(string[] args) {
+      Member m1 = new Member{FirstName = "Ivy", LastName = "Chang", Email = "ivycwq@icloud.com"};
+      Member m2 = new Member{FirstName = "Elham", LastName = "Farhodi", Email = "elhamfarhodi@gmail.com"};
+      Book b1 = new Book{Year = 1988, Title = "I'm b1", Publisher = "b1's Publisher", Authors = new String[]{"Tom", "Susan"}, 
+                         SubjectHeadings = {"hello", "thishey", "fox"}};
+      Book b2 = new Book{Year = 1994, Title = "I'm not a b1 but a b2", Publisher = "b2's Publisher", Authors = new String[]{"a1", "a2"}, 
+                         SubjectHeadings = {"fox", "hey"}};
+      Book b3 = new Book{Year = 2003, Title = "hey", Publisher = "b3's Publisher", Authors = new String[]{"Tom", "Susan"}, 
+                         SubjectHeadings = {"world", "my", "this"}};
+      Library myLibrary = new Library {books = {b1, b2, b3}, members = {m1, m2}};
+      myLibrary.SearchBySubject("hey");
+    }
   }
 }
