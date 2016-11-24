6927c68119ac915144f9f77eee380d996f85858f
Ivy Chang <ivycwq@icloud.com>
2016-11-23 21:01:09 -0800

    adding constructors

--- Book.cs
+++ Book.cs
@@ -3,7 +3,8 @@ using System.Collections.Generic;
 namespace LINQwithUI {
   public class Book {
     private static int nextID = 0;
-    public int ID {get {return nextID;} private set {ID = nextID++;}}
+    private int id;
+    public int ID {get {return id;}}
     public int Year {get; set;}
     public string Title {get;set;}
     public string Publisher {get; set;}
@@ -12,9 +13,17 @@ namespace LINQwithUI {
     public bool IfAvailable {get; set;} = true;
     public int BorrowerID {get; set;}
 
+    public Book(int year, string title, string publisher, string[] authors, List<string> subHeadings) {
+      Year = year;
+      Title = title;
+      Publisher = publisher;
+      Authors = authors;
+      SubjectHeadings = subHeadings;
+      id = ++nextID;
+    }
+
     public override string ToString() {
-      return string.Format("{0, -5}{1, -5}{2, -20}{3, -20", "ID", "Year", "Title", "Authors" +
-                           "{0, -5}{1, -5}{2, -20}{3, -20}", ID, Year, Title, string.Join(", ", Authors));
+      return string.Format("{0, -3}{1, -6}{2, -25}{3, -10}", ID, Year, Title, string.Join(", ", Authors));
     }
   }
 }
--- Library.cs
+++ Library.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 
 namespace LINQwithUI {
   public class Library {
-    private List<Book> books;
-    private List<Member> members;
+    private List<Book> books = new List<Book>();
+    private List<Member> members = new List<Member>();
     public void AddBook(Book b) {
       books.Add(b);
     }
@@ -14,14 +14,17 @@ namespace LINQwithUI {
       members.Add(m);
     }
     public void CheckOut(Book book, int memberId) {
-      book.IfAvailable = false;
-      book.BorrowerID = memberId;
+      if (book.IfAvailable == true) {
+        book.IfAvailable = false;
+        book.BorrowerID = memberId;
+      } else {
+        Console.WriteLine("The book has already been borrowed!");
+      }
+      
     }
     public void SearchBySubject(string searchStr) {
       var query = from book in books
-                  where (book.Title.Contains(searchStr))
-                  from heading in book.SubjectHeadings
-                  where heading.Contains(searchStr)
+                  where (book.Title.Contains(searchStr) || String.Join("", book.SubjectHeadings).Contains(searchStr))
                   select book;
       foreach (Book b in query)
         Console.WriteLine(b);
@@ -31,23 +34,49 @@ namespace LINQwithUI {
                   where (book.BorrowerID == memberId)
                   select book;
 
-      Member m = (Member)members.Select(ID => memberId);
+      Member m = (from member in members
+                        where (member.ID == memberId)
+                        select member).FirstOrDefault();
 
-      Console.WriteLine("Member {0} has borrowed the following books: ", m.ToString());
+      Console.WriteLine("Member {0} has borrowed {1} books.", m.ToString(), bookQuery.Count());
+      foreach (Book b in bookQuery)
+        Console.WriteLine("\t" + b);   
+      
+    }
+    public void SearchAllBooksBorrowed() {
+      var bookQuery = from book in books
+                      where (book.IfAvailable == false)
+                      orderby book.BorrowerID ascending
+                      select book;
       foreach (Book b in bookQuery)
         Console.WriteLine("\t" + b);
     }
     public static void Main(string[] args) {
-      Member m1 = new Member{FirstName = "Ivy", LastName = "Chang", Email = "ivycwq@icloud.com"};
-      Member m2 = new Member{FirstName = "Elham", LastName = "Farhodi", Email = "elhamfarhodi@gmail.com"};
-      Book b1 = new Book{Year = 1988, Title = "I'm b1", Publisher = "b1's Publisher", Authors = new String[]{"Tom", "Susan"}, 
-                         SubjectHeadings = {"hello", "thishey", "fox"}};
-      Book b2 = new Book{Year = 1994, Title = "I'm not a b1 but a b2", Publisher = "b2's Publisher", Authors = new String[]{"a1", "a2"}, 
-                         SubjectHeadings = {"fox", "hey"}};
-      Book b3 = new Book{Year = 2003, Title = "hey", Publisher = "b3's Publisher", Authors = new String[]{"Tom", "Susan"}, 
-                         SubjectHeadings = {"world", "my", "this"}};
-      Library myLibrary = new Library {books = {b1, b2, b3}, members = {m1, m2}};
-      myLibrary.SearchBySubject("hey");
+      Member m1 = new Member("Ivy", "Chang", "ivycwq@icloud.com");
+      Member m2 = new Member("Elham", "Farhodi", "elhamfarhodi@gmail.com");
+    
+      Book b1 = new Book(1988, "I'm b1", "b1's Publisher", new String[]{"Tom", "Susan"}, 
+                         new List<string>{"hello", "thishey", "fox"});
+      Book b2 = new Book(1994, "I'm not a b1 but a b2", "b2's Publisher", new String[]{"a1", "a2"}, 
+                         new List<string>{"fox", "hey"});
+      Book b3 = new Book(2003, "hey", "b3's Publisher", new String[]{"Tom", "Susan"}, 
+                         new List<string>{"world", "my", "this"}); 
+      
+      Library myLibrary = new Library();
+      myLibrary.AddBook(b1);
+      myLibrary.AddBook(b2);
+      myLibrary.AddBook(b3);
+      myLibrary.AddMember(m1);
+      myLibrary.AddMember(m2);
+
+      myLibrary.SearchBySubject("b1");
+
+      myLibrary.CheckOut(b3, 1001);
+      myLibrary.CheckOut(b2, 1001);
+      myLibrary.SearchBooksBorrowed(1001);
+      myLibrary.CheckOut(b1, 1002);
+      Console.WriteLine("All Books Borrowed: ");
+      myLibrary.SearchAllBooksBorrowed();
     }
   }
 }
