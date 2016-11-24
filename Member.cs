a9c0a284ef09bbda987e82a681d00cc118d6c0b8
Ivy Chang <ivycwq@icloud.com>
2016-11-23 21:01:28 -0800

    adding constructors

--- Member.cs
+++ Member.cs
@@ -1,12 +1,19 @@
 namespace LINQwithUI {
   public class Member {
     private static int nextID = 1000;
+    private int id;
+    public int ID {get {return id;}}
     public string FirstName {get; set;}
     public string LastName {get; set;}
-    public int ID {get {return nextID;} private set {ID = nextID++;}}
     public string Email {get; set;}
+    public Member(string fn, string ln, string email) {
+      FirstName = fn;
+      LastName = ln;
+      Email = email;
+      id = ++nextID;
+    }
     public override string ToString() {
-      return string.Format("{0, -5}- {1, -20} {2, -20}, Email: {3, -20}", ID, FirstName, LastName, Email);
+      return string.Format("{0, -5}- {1, -6} {2, -6}, Email: {3, -20}", ID, FirstName, LastName, Email);
     }
   }
 }
