ea579b27223061d1ac1373191cea9ae1b826bf19
Ivy Chang <ivycwq@icloud.com>
2016-11-23 16:30:40 -0800

    adding linq query

--- Library.cs
+++ Library.cs
@@ -1,3 +1,5 @@
+using System.Linq;
+
 using System.Collections.Generic;
 
 namespace LINQwithUI {
@@ -13,6 +15,14 @@ namespace LINQwithUI {
     public void CheckOut(Book book, int memberId) {
       book.IfAvailable = false;
       book.BorrowerID = memberId;
+    }
+    public List<Book> SearchBySubject(string searchStr) {
+      var query = from book in books
+                  where (book.Title.Contains(searchStr))
+                  from heading in book.SubjectHeadings
+                  where heading.Contains(searchStr)
+                  select book;
+      return query.ToList();
     }
   }
 }
